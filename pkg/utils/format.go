61933f20b782e2d764b84c18d0baf50ccad85ab7
Andrey Kovalev <russia9@russia9.dev>
2025-01-14 00:25:02 +0300

    fix: duration length in finance table

--- pkg/utils/format.go
+++ pkg/utils/format.go
@@ -11,11 +11,11 @@ func FormatDuration(d time.Duration) string {
 	hours := int(math.Floor(d.Hours() - float64(days*24)))
 	minutes := int(math.Floor(d.Minutes() - float64(days*24*60) - float64(hours*60)))
 
-	if days >= 10 {
+	if days > 0 {
 		return strconv.Itoa(days) + "d"
 	}
-	if days > 0 {
-		return strconv.Itoa(days) + "d" + strconv.Itoa(hours) + "h"
+	if hours >= 10 {
+		return strconv.Itoa(hours) + "h"
 	}
 	if hours > 0 {
 		return strconv.Itoa(hours) + "h" + strconv.Itoa(minutes) + "m"
